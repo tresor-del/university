0d57e5cf331a32b22209dde9fe4a1ff3076623c8
tresor-del <tresoradzanto7@gmail.com>
2025-08-23 00:21:14 +0000

    finalisation de l'organisation des fichiers. maintenant j'ai un projet structuré

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,18 @@
-from fastapi import FastAPI, ro
+from fastapi import FastAPI
+from .routers import students
+from app.middlewares import setup_middleware
+
+
+
+app = FastAPI()
+
+setup_middleware(app)
+
+app.include_router(students.router)
+
+@app.get(
+    "/",
+    tags=["health"]
+)
+async def root():
+    return {"message": "L'application tourne avec succès"}
