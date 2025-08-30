eeacd591629c0c91d46a06b0db2ebcd496bd3f51
tresor-del <tresoradzanto7@gmail.com>
2025-08-30 06:26:09 +0000

    Ajout du code pour le crud des users

---
+++ backend/app/core/security.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timezone, timedelta
+from typing import Any
+
+import jwt 
+from passlib.context import CryptContext
+from app.core.settings import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else: 
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
---
+++ backend/app/crud/users.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from app.models.users import User
+from app.schemas.users import UserBase, UserInDB, UserRead, UpdateUser
+from app.core.security import verify_password
+
+def create_user(*, db: Session, user_data: UserInDB) -> UserRead | None:
+    db_user = User(**user_data)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(*, db: Session, id: int, data: UpdateUser) -> UserRead | None:
+    user = db.query(User).filter(User.id==id).first()
+    update_data = data.model_dump(exclude_unset=True)
+    for key, value in update_data:
+        setattr(user, key, value)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def get_user(*, db: Session, username: str) -> UserRead | None:
+    statement = select(User).where(User.username==username)
+    user = db.execute(statement=statement).first()
+    return user
+
+def authenticate_user(*, db: Session, username: str, password: str) -> UserRead | None:
+    db_user = db.query(User).filter(User.username==username).first()
+    if not db_user:
+        return None
+    if not verify_password(password, hashed_password=db_user.password):
+        return None
+    return db_user
