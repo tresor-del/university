fdbe711cf93a90e6a26895334567114a00a4a424
tresor-del <tresoradzanto7@gmail.com>
2025-09-07 16:22:15 +0000

    Correction du bug au niveau de test pour tester le headers des utilisateurs normaux

--- backend/app/tests/utils/users.py
+++ backend/app/tests/utils/users.py
@@ -12,12 +12,12 @@ def user_authentication_headers(
     *, 
     client: TestClient, 
     username: str, 
-    password: str
+    password: str,
+    db: Session
 ) -> dict [str: str]:
     data = {"username": username, "password": password}
     r = client.post(f"{settings.API_V1_STR}/login/access-token", data=data)
     response = r.json()
-    print(response)
     auth_token = response["access_token"]
     headers = {"Authorization": f"Bearer {auth_token}"}
     return headers
@@ -41,14 +41,16 @@ def authenticate_user_from_username(
     Si l'utilisateur n'existe pas, il le crée d'abord
     """
     password = random_lower_string()
+    # print(password)
     user = users.get_user_by_username(db=db, username=username)
     if not user:
+        print("user not found")
         user_in_create = UserCreate(username=username, password=password)
         user = users.create_user(db=db, user_data=user_in_create)
-    else: 
+    else:
+        print("user found")
         user_in_update = UserUpdate(password=password)
         if not user.id:
             raise Exception("User not set")
         user = users.update_user(db=db, data=user_in_update, id=user.id)
-        
-    return user_authentication_headers(client=client, username=username, password=password)
+    return user_authentication_headers(db=db,client=client, username=username, password=password)
