b3100939d2d10a9c91be76d94a99318359c4a1a1
tresor-del <tresoradzanto7@gmail.com>
2025-10-14 10:00:00 +0000

    Déplacement du fichier de l'environement virtuel vers le backend

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,27 +1,23 @@
-import jwt , os
-
-from typing import Any
 from datetime import datetime, timezone, timedelta
+from typing import Any
+
+import jwt
 from passlib.context import CryptContext
-from cryptography.fernet import Fernet
 
 from app.core.settings import settings
 
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
 
 ALGORITHM = "HS256"
-
-SECRET_KEY = Fernet.generate_key()
-fernet = Fernet(SECRET_KEY)
-UPLOAD_DIR = settings.MEDIA_UPLOAD_DIRE
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else: 
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -31,6 +27,3 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
-
-
-
